20d1daae351257828a12e16ab6775b4d0359a1aa
Firoz Mohammad <firozm613@gmail.com>
2026-06-17 17:45:12 +0530

    fix: resolve missing closing brace in live_controller.dart

--- lib/features/live/presentation/controllers/live_controller.dart
+++ lib/features/live/presentation/controllers/live_controller.dart
@@ -75,6 +75,7 @@ class LiveController extends GetxController {
           } else if (bodyData['data'] is Map<String, dynamic>) {
             currentSession.value = LiveSessionModel.fromJson(bodyData['data']);
           }
+        }
         if (currentSession.value != null && !currentSession.value!.isBroadcasting) {
           isCameraOn.value = false;
           isAudioOn.value = false;
