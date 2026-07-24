89723d7730a8da3a94552c36a04402e1395006f6
Firoz Mohammad <firozm613@gmail.com>
2026-07-24 14:16:12 +0530

    fix(config): update Astrology API key

--- lib/core/constants/astrology_api_constants.dart
+++ lib/core/constants/astrology_api_constants.dart
@@ -4,7 +4,7 @@ class AstrologyApiConstants {
   static const String baseUrl = 'https://json.astrologyapi.com/v1';
   
   static const String userId = '655788';
-  static const String apiKey = 'ak-2e92dd83da6e067eeb157dbf62a5475802e0cbd0';
+  static const String apiKey = '9d553265802a9777a3cec203872ba41baf8bf58a';
   
   static String get basicAuth {
     final credentials = '$userId:$apiKey';
