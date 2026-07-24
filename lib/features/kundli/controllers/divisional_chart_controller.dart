d5bccaf0b57b8aaba40c048ec5eaec6d57695af0
Firoz Mohammad <firozm613@gmail.com>
2026-07-24 17:44:40 +0530

    feat(kundli): fetch and render direct API SVG charts for all Divisional Chart filters

--- lib/features/kundli/controllers/divisional_chart_controller.dart
+++ lib/features/kundli/controllers/divisional_chart_controller.dart
@@ -6,8 +6,11 @@ class DivisionalChartController extends GetxController {
   final DivisionalChartRepository _repository = DivisionalChartRepository();
 
   var isLoading = false.obs;
+  var isSvgLoading = false.obs;
   var divisionalChartModel = Rxn<DivisionalChartModel>();
   var currentDivision = 1.obs;
+  var northChartSvg = ''.obs;
+  var southChartSvg = ''.obs;
 
   Future<void> fetchDivisionalChart({
     required int division,
@@ -31,6 +34,39 @@ class DivisionalChartController extends GetxController {
       }
     } finally {
       isLoading.value = false;
+    }
+  }
+
+  Future<void> fetchDivisionalChartSvg({
+    required String chartId,
+    required String datetime,
+    required double latitude,
+    required double longitude,
+    required String timezone,
+  }) async {
+    isSvgLoading.value = true;
+    try {
+      final northSvg = await _repository.getHoroChartSvg(
+        chartId: chartId,
+        datetime: datetime,
+        latitude: latitude,
+        longitude: longitude,
+        timezone: timezone,
+        chartType: 'north',
+      );
+      northChartSvg.value = northSvg ?? '';
+
+      final southSvg = await _repository.getHoroChartSvg(
+        chartId: chartId,
+        datetime: datetime,
+        latitude: latitude,
+        longitude: longitude,
+        timezone: timezone,
+        chartType: 'south',
+      );
+      southChartSvg.value = southSvg ?? '';
+    } finally {
+      isSvgLoading.value = false;
     }
   }
 }
