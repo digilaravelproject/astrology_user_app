7f3f89f71338a7e8f058e320e537bac35ffcb2c9
Firoz Mohammad <firozm613@gmail.com>
2026-07-24 15:20:43 +0530

    fix(kundli): remove Yogini Dasha from dasha_controller.dart

--- lib/features/kundli/controllers/dasha_controller.dart
+++ lib/features/kundli/controllers/dasha_controller.dart
@@ -1,15 +1,12 @@
 import 'package:get/get.dart';
 import '../models/dasha_model.dart';
-import '../models/yogini_dasha_model.dart';
 import '../repositories/dasha_repository.dart';
 
 class DashaController extends GetxController {
   final DashaRepository _repository = DashaRepository();
   
   var isLoading = false.obs;
-  var isYoginiLoading = false.obs;
   var dashaModel = Rxn<DashaModel>();
-  var yoginiDashaModel = Rxn<YoginiDashaModel>();
 
   Future<void> fetchDashaDetails({
     required String datetime,
@@ -28,26 +25,6 @@ class DashaController extends GetxController {
       dashaModel.value = result;
     } finally {
       isLoading.value = false;
-    }
-  }
-
-  Future<void> fetchYoginiDashaDetails({
-    required String datetime,
-    required double latitude,
-    required double longitude,
-    String timezone = "+05:30",
-  }) async {
-    isYoginiLoading.value = true;
-    try {
-      final result = await _repository.getYoginiDashaDetails(
-        datetime: datetime,
-        latitude: latitude,
-        longitude: longitude,
-        timezone: timezone,
-      );
-      yoginiDashaModel.value = result;
-    } finally {
-      isYoginiLoading.value = false;
     }
   }
 }
