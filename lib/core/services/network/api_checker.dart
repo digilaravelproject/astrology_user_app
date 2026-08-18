1d69a67b4ac771827694f94883907bf065d0b6af
Firoz Mohammad <firozm613@gmail.com>
2026-08-18 15:25:38 +0530

    fix: resolve DioExceptionType exhaustiveness and default_res errors

--- lib/core/services/network/api_checker.dart
+++ lib/core/services/network/api_checker.dart
@@ -246,7 +246,7 @@ class ApiChecker {
             message: 'No internet connection',
           );
 
-        case DioExceptionType.unknown:
+        default:
           if (showErrorScreen) {
             _showErrorScreen(
               title: 'Unknown Error',
