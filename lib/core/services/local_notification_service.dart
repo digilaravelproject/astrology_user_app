0867b01900315e6489d382fb4506cf792dc3346c
Firoz Mohammad <firozm613@gmail.com>
2026-08-22 02:44:50 +0530

    Fix Get.keys notification translation check

--- lib/core/services/local_notification_service.dart
+++ lib/core/services/local_notification_service.dart
@@ -385,14 +385,8 @@ class LocalNotificationService {
     String translatedTitle = title;
     String translatedBody = body;
     try {
-      if (Get.keys.containsKey(Get.locale?.toString()) &&
-          Get.keys[Get.locale?.toString()]?.containsKey(title) == true) {
-        translatedTitle = title.tr;
-      }
-      if (Get.keys.containsKey(Get.locale?.toString()) &&
-          Get.keys[Get.locale?.toString()]?.containsKey(body) == true) {
-        translatedBody = body.tr;
-      }
+      translatedTitle = title.tr;
+      translatedBody = body.tr;
     } catch (_) {}
 
     await _notificationsPlugin.show(
