3ad0de27280bd81ab1ac2fcfbbfd3ffe394b1e99
Firoz Mohammad <firozm613@gmail.com>
2026-08-26 17:59:53 +0530

    fix: Resolve CustomSnackbar import in chat and call list screens

--- lib/features/chat/presentation/pages/chat_list_screen.dart
+++ lib/features/chat/presentation/pages/chat_list_screen.dart
@@ -1,3 +1,5 @@
+import 'package:astro_user/core/utils/custom_snackbar.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
